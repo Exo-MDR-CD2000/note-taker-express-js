d347e333e425f7d1ce6a381b66113b8997d5db8f
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-08-25 16:12:17 -0700

    added uuid package for unique id's

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # note-taker-express-js
 A web application for simple note taking
+
+
+
+
+
+
+
+
+
+
+## Credits
+
+- Node UUID package for [Unique Id's](https://www.npmjs.com/package/uuid)
--- server.js
+++ server.js
@@ -3,3 +3,15 @@
 //front end code is already written.
 // i need to develop the back end so that the front end can communicate with it.
 // more specifically, the note taker app must be able to save, retrieve, and delete notes from a json file.
+
+// the first issue is that the app 'Cannot GET /notes' when clicking on the 'Get Started' button.
+
+// i guess I can add soe boilerplate code to the server.js file to get started.
+
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
