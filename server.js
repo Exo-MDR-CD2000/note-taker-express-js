dbe0c4cbbac5aa5e5c341cde6ac5ec64ae5e0ed6
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-08-25 15:37:32 -0700

    instructions for TODO

--- server.js
+++ server.js
@@ -1,1 +1,5 @@
 // code to start the server goes here
+
+//front end code is already written.
+// i need to develop the back end so that the front end can communicate with it.
+// more specifically, the note taker app must be able to save, retrieve, and delete notes from a json file.
