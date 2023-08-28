b51a5352831553878545f1f994ab3e4cf62e3e10
Exo-MDR-CD2000 <joseguillen587@yahoo.com>
2023-08-27 21:13:56 -0700

    saveNotes test

--- routes/saveNotes.js
+++ routes/saveNotes.js
@@ -1,1 +1,51 @@
 //define the POST route for saving notes.
+
+const saveNotes = require('express').Router();
+
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require("uuid");
+const existingNotes = require('./getNotes');
+
+
+//forgot to import the getNotes route.
+
+
+// //let's retrieve the notes from the db.json file.
+
+// saveNotes.get('/', (req, res) => {
+//     const data = fs.readFileSync('./db/db.json', 'utf8');
+//     res.json(JSON.parse(data));
+// });
+
+
+// now I need to deconstruct the json data, add a unique id to the note, then add the new note to the existing notes.
+
+
+saveNotes.post('/', (req, res) => {
+    
+    const { title, text } = req.body;
+
+    if (title && text) {
+        
+        const newNote = {
+            title,
+            text,
+            noteId: uuidv4()
+        };
+
+        fs.writeFile('./db/db.json', JSON.stringify(newNote), (err) => {
+            if (err) throw err;
+            console.log('Note added!');
+        });
+
+        const response = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(response);
+    } else {
+        res.json('Error in posting note');
+    }
+});
